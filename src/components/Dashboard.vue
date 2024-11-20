47c2fab5465a4584271417ec1f73f73c475988f6
Dominic Pham <dominicpham@Dominics-Air.attlocal.net>
2024-11-19 23:43:51 -0800

    Change

---
+++ src/components/Dashboard.vue
@@ -0,0 +1,24 @@
+<template> 
+    <div class="dashboard"> 
+    <ul>
+        <li v-for="item in objectItems">{{ item }}</li>
+  </ul>
+    </div>
+</template>
+
+
+
+<script>
+
+
+
+</script>
+
+<style scoped>
+.dashboard{
+    width: 885px;
+    height: 100%;
+}
+
+
+</style>
